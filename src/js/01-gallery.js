791534cb36cdcdec7a2be8dabc5b1701599be736
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-12 23:19:58 +0200

    js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const ulGallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}"
+                    />
+                </a>
+            </li>`;
+  })
+  .join('');
+
+ulGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  navText: ['<', '>'],
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+ulGallery.addEventListener('click', lightbox);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,37 @@
 import throttle from 'lodash.throttle';
-console.log(throttle);
+
+refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const formTotal = refs.form;
+const inputEmail = refs.form.elements.email;
+const inputMessage = refs.form.elements.message;
+const inputData = {};
+
+formTotal.addEventListener('input', throttle(handlerFormInput), 500);
+function handlerFormInput(evt) {
+  const emailText = inputEmail.value;
+  inputData.email = emailText;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+  const messageText = inputMessage.value;
+  inputData.message = messageText;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+}
+
+formTotal.addEventListener('submit', handlerFormSubmit);
+function handlerFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(inputData);
+  localStorage.removeItem('feedback-form-state');
+  evt.target.reset();
+}
+
+function onLoad() {
+  const jsonData = localStorage.getItem('feedback-form-state');
+  const jsonInput = JSON.parse(jsonData) || {};
+
+  inputEmail.value = jsonInput.email || '';
+  inputMessage.value = jsonInput.message || '';
+}
+onLoad();
