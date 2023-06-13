4e25d4a99ffde01e76a1efc35ad8a8cad477fb14
GKastet <127600582+GKastet@users.noreply.github.com>
2023-06-13 16:56:52 +0200

    inputData, onLoad

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,34 +7,26 @@ const refs = {
 const formTotal = refs.form;
 const inputEmail = refs.form.elements.email;
 const inputMessage = refs.form.elements.message;
-const inputData = {};
+let inputData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 
 formTotal.addEventListener('input', throttle(handlerFormInput), 500);
 function handlerFormInput(evt) {
-  const emailText = inputEmail.value;
-  inputData.email = emailText;
-  const messageText = inputMessage.value;
-  inputData.message = messageText;  
+  inputData[evt.target.name] = evt.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
 }
 
 
 function onLoad() {
-  const jsonData = localStorage.getItem('feedback-form-state');
-  const jsonInput = JSON.parse(jsonData) || {};
-  inputEmail.value = jsonInput.email || '';
-  inputMessage.value = jsonInput.message || '';
+  inputEmail.value = inputData.email || '';
+  inputMessage.value = inputData.message || '';
 }
 onLoad();
 
 formTotal.addEventListener('submit', handlerFormSubmit);
 function handlerFormSubmit(evt) {
   evt.preventDefault();
-  const emailText = inputEmail.value;
-  inputData.email = emailText;
-  const messageText = inputMessage.value;
-  inputData.message = messageText;
   console.log(inputData);
   localStorage.removeItem('feedback-form-state');
   evt.target.reset();
+  inputData = {};
 }
